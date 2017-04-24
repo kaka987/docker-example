6c4418b6d7500dcda6bb54524a1a30b1bd6101c3
kaka987 <zxf.0810@163.com>
2017-04-24 17:29:16 +0800

    add version

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+version=`cat VERSION`
 
-sudo docker build -t='192.168.100.99:5000/php7:latest' php
+sudo docker build -t="192.168.100.99:5000/php7:${version}" php
 sleep 2
 
-sudo docker push 192.168.100.99:5000/php7:latest
+sudo docker push 192.168.100.99:5000/php7:${version}
 sleep 2
 
 echo "build ok."
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+CATTLE_ACCESS_KEY='4D2615F51BA0F72DE896'
+CATTLE_SECRET_KEY='L2vr25Gawd53ZbCRJvm4AVUJiQU9jCijUkUvybiR'
+curl -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" \
+	-X POST \
+	-H 'Accept: application/json' \
+	-H 'Content-Type: application/json' \
+	-d '{}' \
+	'http://192.168.100.100:8080/v2-beta/projects/1a5/services/1s36/?action=update'
 echo 'deploy ok'
