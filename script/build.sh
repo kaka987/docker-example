2763b96ce587f306bf36683466cb180e05055e04
kaka987 <zxf.0810@163.com>
2017-04-24 16:25:29 +0800

    update

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/usr/bin/docker build -t='192.168.100.99:5000/php7:latest' php
+sudo docker build -t='192.168.100.99:5000/php7:latest' php
 sleep 2
 
-/usr/bin/docker push 192.168.100.99:5000/php7:latest
+sudo docker push 192.168.100.99:5000/php7:latest
 sleep 2
 
 echo "build ok."
