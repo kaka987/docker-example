8583b7507dea99f53709fcb1d7d12e3e039d3387
kaka987 <zxf.0810@163.com>
2017-04-24 23:19:07 +0800

    update build.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,14 +10,12 @@ curl -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" \
 	-H 'Content-Type: application/json' \
 	-d '{}' \
 	'http://192.168.100.100:8080/v2-beta/projects/1a5/services/1s39/?action=finishupgrade'
-
 echo 
 echo '---------------------------------'
-
+sleep 5
 curl -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" \
 	-X POST \
 	-H 'Accept: application/json' \
 	-H 'Content-Type: application/json' \
 	-d '{"inServiceStrategy":{"launchConfig":{"imageUuid":"docker:'${docker_image}'"}}, "toServiceStrategy":null}' \
 	'http://192.168.100.100:8080/v2-beta/projects/1a5/services/1s39/?action=upgrade'
-echo 'deploy ok'
