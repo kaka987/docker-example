1d13518b7a511f8b91cbf57f3205ff04520bdbc4
kaka987 <zxf.0810@163.com>
2017-04-24 18:10:02 +0800

    update build.sh

--- script/build.sh
+++ script/build.sh
@@ -2,7 +2,7 @@
 version=`cat script/VERSION`
 docker_image="192.168.100.99:5000/php7:${version}"
 
-sudo docker --no-cache=true build -t="${docker_image}" php
+sudo docker build --no-cache=true -t="${docker_image}" php
 sleep 1
 sudo docker push ${docker_image}
 sleep 1
