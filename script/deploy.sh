801dc853cc3fff5207681174bb6b5ebaf67cd655
kaka987 <zxf.0810@163.com>
2017-04-24 22:12:26 +0800

    update build.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 CATTLE_ACCESS_KEY='4D2615F51BA0F72DE896'
 CATTLE_SECRET_KEY='L2vr25Gawd53ZbCRJvm4AVUJiQU9jCijUkUvybiR'
+version=`cat script/VERSION`
+docker_image="192.168.100.99:5000/php7:${version}"
+
 curl -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" \
 	-X POST \
 	-H 'Accept: application/json' \
 	-H 'Content-Type: application/json' \
-	-d '{}' \
-	'http://192.168.100.100:8080/v2-beta/projects/1a5/services/1s36/?action=update'
+	-d '{"inServiceStrategy":{"launchConfig":{"imageUuid":"docker:${docker_image}"}}, "toServiceStrategy":null}' \
+	'http://192.168.100.100:8080/v2-beta/projects/1a5/services/1s38/?action=upgrade'
 echo 'deploy ok'
